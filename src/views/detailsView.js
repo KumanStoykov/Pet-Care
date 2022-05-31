fd4d59883050c5dddaeda78a7f816975c6cfa120
Kuman Stoykov <kumanstoykov@gmail.com>
2022-05-31 21:22:12 +0200

    Update requests

--- src/views/detailsView.js
+++ src/views/detailsView.js
@@ -18,62 +18,63 @@ const detailsTemplate = (pet, onDelete, onDonate) => html`
             </div>
             
             ${pet.isLogged
-            ? isOwnerTemplate(pet, onDelete, onDonate)                   
-            : nothing
-            }
+        ? isOwnerTemplate(pet, onDelete, onDonate)
+        : nothing
+    }
 
         </div>
     </div>
 </section>`;
 
-const isOwnerTemplate = (pet, onDelete, onDonate) => html `
+const isOwnerTemplate = (pet, onDelete, onDonate) => html`
     <div class="actionBtn">
     ${pet.isOwner
-    ? html ` <a href="/edit/${pet._id}" class="edit">Edit</a>
+        ? html` <a href="/edit/${pet._id}" class="edit">Edit</a>
             <a @click=${onDelete} href="javascript:void(0)" class="remove">Delete</a>`
 
-    : donateBtnTemplate(pet, onDonate)
+        : donateBtnTemplate(pet, onDonate)
     }                    
     </div>`;
 
-const donateBtnTemplate = (pet, onDonate) => html `
+const donateBtnTemplate = (pet, onDonate) => html`
     ${pet.hasDonate
-    ? nothing
-    : html `<a @click=${onDonate} href="/details/${pet._id}" class="donate">Donate</a>`
+        ? nothing
+        : html`<a @click=${onDonate} href="/details/${pet._id}" class="donate">Donate</a>`
     }`;
 
 export const detailsView = async (context) => {
     const petId = context.params.id;
     const user = context.user;
 
-    const [pet, countOfDonation] = await Promise.all ([
-        petService.loadCurrentPet(petId),
-        donationService.getCountOfDonation(petId)
-    ]);
+
+    let pet = await petService.loadCurrentPet(petId);
+    let countOfDonation = await donationService.getCountOfDonation(petId);
+
 
     const hasDonate = await donationService.hasDonate(pet._id, user?.id);
+
 
     const donation = countOfDonation * 100;
 
     pet.donation = donation;
-    pet.isLogged = Boolean(user);    
+    pet.isLogged = Boolean(user);
     pet.isOwner = Boolean(user?.id == pet._ownerId);
     pet.hasDonate = Boolean(hasDonate);
 
-    
+
     const onDelete = async () => {
         const dialog = confirm('Delete!!!');
 
         if (dialog) {
-           await petService.deletePet(petId);
-                
-            context.page.redirect('/');               
+            await petService.deletePet(petId);
+
+            context.page.redirect('/');
         }
     }
 
     const onDonate = async () => {
         await donationService.addDonation({ petId });
-       
+
     }
     context.render(detailsTemplate(pet, onDelete, onDonate));
 }
